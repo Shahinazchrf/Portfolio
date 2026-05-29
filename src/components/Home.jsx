705d409fc41ec091e60b36c1b7c5cdba3b013173
shahyy <chahinaz.cherfi@univ-alger.dz>
2026-05-29 16:30:28 +0100

    Update Home with correct image path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,9 +71,6 @@ LANGUAGES
               <img 
                 src="/Portfolio/assets/IMG_20251207_201927_364-Dvu4Oqzd.jpg"
                 alt="Chahinez Cherfi"
-                onError={(e) => {
-                  e.target.src = "https://randomuser.me/api/portraits/women/68.jpg";
-                }}
               />
             </div>
           </div>
